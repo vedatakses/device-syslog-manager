d945b2773f7b8519c6a0ce922eae0c95349f3de2
Vedat Akses <vedatakses@gmail.com>
2017-01-09 21:25:04 +0200

    Add controller logger

--- src/main/java/com/syslog/controller/SyslogController.java
+++ src/main/java/com/syslog/controller/SyslogController.java
@@ -2,13 +2,15 @@ package com.syslog.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.http.MediaType;
 import com.syslog.data.Message;
 import com.syslog.repository.MessageRepository;
 
@@ -16,17 +18,21 @@ import com.syslog.repository.MessageRepository;
 @RequestMapping(value = "/logs")
 public class SyslogController {
 
+	private static final Logger log = LoggerFactory.getLogger(SyslogController.class);
+	
 	@Autowired
 	MessageRepository repository;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Message findById(@PathVariable("id") final Long id) {
+		log.info("log - findById: " + id);
 		return repository.findMessage(Long.toString(id));
 	}
 
 	@SuppressWarnings("unchecked")
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Message> findAll() {
+		log.info("log - findAll");
 		return (List<Message>) repository.findAllMessages();
 	}
 
