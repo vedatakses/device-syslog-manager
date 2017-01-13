321adf63db7a0140884486375fc05d4b36b87bcd
Vedat Akses <vedatakses@gmail.com>
2017-01-13 12:38:13 +0300

    Add health controller

---
+++ src/main/java/com/syslog/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.syslog.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@Value("${service.name}")
+	private String serviceName;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return "syslog manager is running..";
+	}
+}
