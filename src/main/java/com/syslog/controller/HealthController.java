cbac356014384223ebe17bd68326d323e89c1e63
Vedat Akses <vedatakses@gmail.com>
2017-01-15 20:34:37 +0200

    makeup

--- src/main/java/com/syslog/controller/HealthController.java
+++ src/main/java/com/syslog/controller/HealthController.java
@@ -13,6 +13,6 @@ public class HealthController {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
-		return "Hallo, Welt from + !" + serviceName;
+		return "Hallo, Welt from " + serviceName + "!";
 	}
 }
